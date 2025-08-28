1ed0e3ce00be2a9a6971f8c5e5133def8e91960d
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2025-08-28 12:18:19 +0000

    Add more images

--- src/index.js
+++ src/index.js
@@ -4,15 +4,20 @@ import "./styles/styles.css";
 import image1 from "./assets/images/01.jpg";
 import image2 from "./assets/images/02.jpg";
 import image3 from "./assets/images/03.jpg";
+import image4 from "./assets/images/04.jpg";
+import image5 from "./assets/images/05.jpg";
+import image6 from "./assets/images/06.jpg";
+import image7 from "./assets/images/07.jpg";
+import image8 from "./assets/images/08.jpg";
 
 console.log("test");
 
-const allImages = [image1, image2, image3];
+const allImages = [image1, image2, image3, image4, image5, image6, image7, image8];
 
 const wideCont = document.querySelector(".wide-container");
 const navDots = document.querySelector(".nav-dots");
 
-const xlist = [400, 0, -400];
+const xlist = [1400, 1000, 600, 200, -200, -600, -1000, -1400];
 let index = 0;
 
 for (const img of allImages) {
@@ -25,13 +30,15 @@ for (const img of allImages) {
   dot.classList.add("dot");
   navDots.append(dot);
   dot.id = "index-" + index;
-  if (index === 1) {
+  if (index === 0) {
     dot.classList.add("curr");
   }
   index = (index + 1) % xlist.length;
 }
 
-index = 1;
+wideCont.style.translate = xlist[0] + "px";
+
+index = 0;
 
 const prevBtn = document.querySelector("#prev-btn");
 const nextBtn = document.querySelector("#next-btn");
