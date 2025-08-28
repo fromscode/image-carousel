7838e9f79c4c187b4de8ba6c5c4fa557d41b103f
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2025-08-28 10:25:06 +0000

    Add container and images

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import "./styles/styles.css";
 import "./styles/reset.css";
 
+import image1 from "./assets/images/01.jpg";
+import image2 from "./assets/images/02.jpg";
+import image3 from "./assets/images/03.jpg";
+
 console.log("test");
+
+const allImages = [image1, image2, image3];
+
+const wideCont = document.querySelector(".wide-container");
+
+for (const img of allImages) {
+  const elemnt = document.createElement("img");
+  elemnt.src = img;
+  elemnt.height = 300;
+  wideCont.append(elemnt);
+}
